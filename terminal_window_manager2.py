8824ca3c81f7fda40f05e497038de1aec090207c
Loïc Villet <villet.loic@gmail.com>
2024-01-23 03:33:37 +0100

    actually making progress, waaaaw

--- terminal_window_manager2.py
+++ terminal_window_manager2.py
@@ -2,6 +2,11 @@ import time
 import json
 import pygetwindow as gw
 import os
+import re
+
+# read dict from JSON file
+with open("terminal_window_dict.json", "r") as json_file:
+    my_dict = json.load(json_file)
 
 
 # transform the terminal window so that it fits my screen nicely, on a second monitor
@@ -24,22 +29,37 @@ def resize_and_move_window(window_title, new_width, new_height, new_x, new_y):
 
 
 def adjust_terminal_window():
+    # assign a slot to the cmd window that just spawned and rename it accordingly.
+    slot = get_first_free_slot_in_dict()
+    my_dict[slot] = "MY TERMINAL NAME - " + str(slot)
+    new_window_title = my_dict[slot]  # take the value of the key as a name for the window
+    os.system(f"title {new_window_title}")  # renames the window with a shell command
+
+    # transform and replace the window according to it's occupied slot number, obtained by removing non-digits char.
+    slot_number = int(re.sub(r'\D', "", slot))
+
     width = 600
     height = 300
     x_pos = -600
-    y_pos = 300
-
-    # change the terminal window title relative to the number of windows open to allow for unique identification.
-    new_window_title = f"MY TERMINAL NAME)"
-    os.system(f"title {new_window_title}")
+    y_pos = 300 * slot_number
 
     resize_and_move_window(new_window_title, width, height, x_pos, y_pos)
 
+    time.sleep(0.1)
+    free_dict_value(slot)
 
-# def create_dict():
-#     # here make the dictionary, with (slot{i} : "Title") pairs
-#     slots = {"slot" + str(i): "FREE" for i in range(1, 10)}
-#     return slots
+
+def create_dict():
+    # here make the dictionary, with (slot{i} : "Title") pairs
+    slots = {"slot" + str(i): "FREE" for i in range(0, 9)}
+    return slots
+
+
+def get_first_free_slot_in_dict():
+    for slot, status in my_dict.items():
+        if status == "FREE":
+            print(slot, f"was the first open slot available")
+            return slot
 
 
 def write_dict():
@@ -47,7 +67,10 @@ def write_dict():
         json.dump(my_dict, file)
 
 
-with open ("terminal_window_dict.json", "r") as json_file:
-    my_dict = json.load(json_file)
+def free_dict_value(key):
+    my_dict[key] = "FREE"
+    input("press enter to close and free the dict entry")
 
+
+adjust_terminal_window()
 write_dict()
