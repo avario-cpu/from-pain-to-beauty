71e0b643450fd7f765092e816c2c5c4fe72a86d8
Loïc Villet <villet.loic@gmail.com>
2024-02-02 20:08:28 +0100

    last cleanup, back to dev now

--- test_folder/start_server2.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-title SERVER
-cd /d "C:\Users\ville\MyStreamingData\Scripts\Python-Mega-Script\venv\Scripts\"
-call .\activate
-cd /d "C:\Users\ville\MyStreamingData\Scripts\Python-Mega-Script"
-py server.py
