2901fad45343ac6c412c64f091888bb3eb620b30
Loïc Villet <villet.loic@gmail.com>
2024-06-24 22:54:34 +0200

    Implement pregame full detection
    Framework for the ws communication is also being set in place :)

--- game_start_watcher.py
+++ game_start_watcher.py
@@ -31,15 +31,19 @@ class PregameSate:
         self.hero_pick = False
         self.starting_buy = False
         self.versus_screen = False
+        self.in_game = False
 
 
 HERO_PICK_CAPTURE_AREA = {"left": 790, "top": 140, "width": 340, "height": 40}
-HERO_PICK_TEMPLATE_PATH = "opencv/dota_select_your_hero_message.jpg"
-SECONDARY_WINDOWS = [my.SecondaryWindow("opencv_hero_pick_scanner", 350, 100),
-                     my.SecondaryWindow("test", 150, 150)]
+HERO_PICK_TEMPLATE_PATH = "opencv/dota_choose_your_hero_message.jpg"
+START_BUY_CAPTURE_AREA = {"left": 880, "top": 70, "width": 160, "height": 30}
+START_BUY_TEMPLATE_PATH = "opencv/dota_strategy_time_message.jpg"
+IN_GAME_CAPTURE_AREA = {"left": 1820, "top": 1020, "width": 80, "height": 60}
+IN_GAME_TEMPLATE_PATH = "opencv/dota_deliver_items_icon.jpg"
+SECONDARY_WINDOWS = [my.SecondaryWindow("opencv_hero_pick_scanner", 350, 100)]
 SCRIPT_NAME = "dota2_pregame_detector"
 STREAMERBOT_WS_URL = "ws://127.0.0.1:50001/"
-stop_hero_pick_loop = asyncio.Event()
+stop_main_loop = asyncio.Event()
 secondary_windows_have_spawned = asyncio.Event()
 mute_main_loop_print_feedback = asyncio.Event()
 stop_subprocess = asyncio.Event()
@@ -99,16 +103,25 @@ async def compare_images(image_a, image_b):
     return ssim(image_a, image_b)
 
 
-async def send_camera_adjust_request(ws):
+async def send_camera_adjust_request(ws, game_state):
+    if game_state.in_game:
+        pass
+    elif game_state.versus_screen:
+        pass
+    elif game_state.starting_buy:
+        pass
+    elif game_state.hero_pick:
+        pass
     pass
 
 
-async def detect_hero_pick_phase(ws):
+async def detect_pregame_phase(ws):
     game_sate = PregameSate()
     template = cv.imread(HERO_PICK_TEMPLATE_PATH, cv.IMREAD_GRAYSCALE)
+    capture_area = HERO_PICK_CAPTURE_AREA
 
-    while not stop_hero_pick_loop.is_set():
-        frame = await capture_window(HERO_PICK_CAPTURE_AREA)
+    while not stop_main_loop.is_set():
+        frame = await capture_window(capture_area)
         gray_frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
         match_value = await compare_images(gray_frame, template)
         cv.imshow(SECONDARY_WINDOWS[0].name, gray_frame)
@@ -118,17 +131,39 @@ async def detect_hero_pick_phase(ws):
             break
         if not mute_main_loop_print_feedback.is_set():
             print(f"SSIM: {match_value:.6f}", end="\r")
-        if match_value >= 0.8:
+        if match_value >= 0.8 and not game_sate.hero_pick:
             game_sate.hero_pick = True
             print("Hey! You're picking :)")
-            await send_camera_adjust_request(ws)
-            await asyncio.sleep(1)
-        await asyncio.sleep(0.01)
+            capture_area = START_BUY_CAPTURE_AREA
+            template = cv.imread(START_BUY_TEMPLATE_PATH, cv.IMREAD_GRAYSCALE)
+            await send_camera_adjust_request(ws, game_sate)
+
+        elif match_value >= 0.8 and not game_sate.starting_buy:
+            game_sate.starting_buy = True
+            print("Oh, now this is the starting buy !")
+            await send_camera_adjust_request(ws, game_sate)
+
+        elif (match_value <= 0.8
+              and game_sate.hero_pick
+              and game_sate.starting_buy
+              and not game_sate.versus_screen):
+            game_sate.versus_screen = True
+            print("Hey look, you're in VS screen :)")
+            capture_area = IN_GAME_CAPTURE_AREA
+            template = cv.imread(IN_GAME_TEMPLATE_PATH, cv.IMREAD_GRAYSCALE)
+            await send_camera_adjust_request(ws, game_sate)
+
+        elif (match_value >= 0.8
+              and game_sate.versus_screen
+              and not game_sate.in_game):
+            game_sate.in_game = True
+            print("Woah ! You're in game now !")
+            await send_camera_adjust_request(ws, game_sate)
+
+    await asyncio.sleep(0.01)
 
 
 async def main():
-    """If there are no single instance lock file, start the Dota2 shop_watcher
-     module. Reposition the terminal right at launch."""
     if single_instance.lock_exists(SCRIPT_NAME):
         slot = twm.manage_window(twm.WinType.DENIED, SCRIPT_NAME)
         atexit.register(denied_sdh.free_slot, slot)
@@ -146,7 +181,7 @@ async def main():
         ws = None
         try:
             ws = await establish_ws_connection()
-            main_task = asyncio.create_task(detect_hero_pick_phase(ws))
+            main_task = asyncio.create_task(detect_pregame_phase(ws))
             await secondary_windows_have_spawned.wait()
             twm.manage_secondary_windows(slot, SECONDARY_WINDOWS)
             mute_main_loop_print_feedback.clear()
