58104ed6c92c3030e9f874258aa7408d5b455a0d
Loïc Villet <villet.loic@gmail.com>
2024-07-08 20:24:46 +0200

    Develop neo4j interactions Added different random pool group for grouped nodes responses

--- src/utils/neo4j_local_test_speed.py
+++ src/utils/neo4j_local_test_speed.py
@@ -1,22 +1,10 @@
 from neo4j import GraphDatabase
 from neo4j import Session
 import time
-
-# Replace with your local Neo4j connection details
-uri = "bolt://localhost:7687"
-username = "neo4j"
-password = "adw[@#$u392a"  # Replace with your actual password
-
-# Measure connection establishment time
-start_time = time.time()
-driver = GraphDatabase.driver(uri, auth=(username, password))
+from collections import defaultdict
 
 
-# Reuse session
-session = driver.session()
-
-
-def get_node_data(neo4j_session, prompt_text):
+def get_node_connections(neo4j_session, prompt_text):
     start_time = time.time()
     result = neo4j_session.run(
         """
@@ -26,13 +14,10 @@ def get_node_data(neo4j_session, prompt_text):
         """,
         prompt_text=prompt_text,
     )
-    elapsed_time = time.time() - start_time
-
     # Define the data structure to store the result
     result_data = []
-    connections_list = []
 
-    for record in result:
+    for index, record in enumerate(result):
         p = record["p"]
         R = record["R"]
         x = record["x"]
@@ -46,52 +31,98 @@ def get_node_data(neo4j_session, prompt_text):
         response_labels = list(x.labels)
         response_props = dict(x)
 
-        connection_string = (
-            f'"{prompt_props['text']}" '  # the node with this matching text value...
-            f'{relationship_type} '  # TRIGGERS/UNLOCKS...
-            f'"{response_props['text']}"'  # this other node, containing this text value...
-            f' (params: {relationship_props})'  # according to those parameters.
-        )
-
         connection = {
+            "id": index,
             "prompt": prompt_props["text"],
             "relationship": relationship_type,
             "response": response_props.get("text", "N/A"),
-            "params": relationship_props
+            "audio": response_props.get("audioFile", "N/A"),
+            "params": relationship_props,
         }
-        
-        connections_list.append(connection_string)
+
         result_data.append(connection)
-
-    for i in range(len(connections_list)):
-        print(connections_list[i], end='\n')
-    elapsed_time = time.time() - start_time
-    print(f"Query + processing time: {elapsed_time:.3f} seconds")
-
     return result_data
 
 
+def group_by_id(tuples_list):
+    grouped_data = defaultdict(list)
+
+    for id, value in tuples_list:
+        grouped_data[id].append(value)
+
+    # Convert the grouped data to a list of lists
+    result = list(grouped_data.values())
+    return result
+
+
+def process_triggers(trigger_ids, connections):
+    random_triggers = []
+    guaranteed_triggers = []
+
+    for id in trigger_ids:
+        is_random = connections[id]["params"]["isRandom"]
+        if is_random:
+            random_pool_id = connections[id]["params"]["randomPoolId"]
+            random_triggers.append((random_pool_id, connections[id]))
+
+    random_triggers = group_by_id(random_triggers)
+    for trigger_group in random_triggers:
+        print("\n")
+        print(trigger_group, end="\n")
+
+
+def process_relationships(connections):
+    trigger_connections = []
+    for i in range(len(connections)):
+        relationship = connections[i]["relationship"]
+        if relationship == "TRIGGERS":
+            id = connections[i]["id"]
+            trigger_connections.append(id)
+    process_triggers(trigger_connections, connections)
+
+
 def parse_string(session: Session, str: str):
-    result = get_node_data(session, prompt_text=str)
-    for i in range(len(result)):
-        print(result[i], end='\n')
+    connections = get_node_connections(session, prompt_text=str)
+    for i in range(len(connections)):
+        print(connections[i], end="\n")
+    process_relationships(connections)
 
 
-try:
-    session.run("RETURN 1").consume()  # Warmup query
-    connection_time = time.time() - start_time
-    print(f'Connection established in {connection_time:.3f} seconds')
+def main():
+    uri = "bolt://localhost:7687"
+    username = "neo4j"
+    password = "adw[@#$u392a"
 
-    while True:
-        user_input = (
-            input("Enter target node text value to run the query or 'exit' to quit: ").strip().lower()
-        )
-        if user_input == "exit":
-            print("Exiting...")
-            break
-        else:
-            parse_string(session=session, str=user_input)
-finally:
-    # Close the session and driver
-    session.close()
-    driver.close()
+    # Measure connection establishment time
+    start_time = time.time()
+    driver = GraphDatabase.driver(uri, auth=(username, password))
+
+    # Reuse session
+    session = driver.session()
+
+    try:
+        session.run("RETURN 1").consume()  # Warmup query
+        connection_time = time.time() - start_time
+        print(f"Connection established in {connection_time:.3f} seconds")
+
+        while True:
+            user_input = (
+                input(
+                    "Enter 'hello' (or different node text value) to run the query or 'exit' to quit: "
+                )
+                .strip()
+                .lower()
+            )
+            if user_input == "exit":
+                print("Exiting...")
+                break
+            else:
+                parse_string(session=session, str=user_input)
+    finally:
+        # Close the session and driver
+        session.close()
+        driver.close()
+
+
+if __name__ == "__main__":
+    main()
