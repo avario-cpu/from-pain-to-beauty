e18f747d22850b84aa1b237e57eae233cfff5638
Loïc Villet <villet.loic@gmail.com>
2025-07-17 01:34:30 +0200

    refactor: Make local server lauch scripts path agnostic

--- run_server.bat
+++ run_server.bat
@@ -1,8 +1,6 @@
-REM script to run "start_server_script.ps1" as an executable
+REM script ued to run "start_server_script.ps1" as an executable from StreamDeck
 
 @echo off
-REM Set the path to the PowerShell script
-set PSScriptPath="C:\Users\ville\MyMegaScript\start_server_script.ps1"
-
-REM Execute the PowerShell script
+REM Use the directory where this batch file is located
+set PSScriptPath="%~dp0start_server_script.ps1"
 start powershell -NoProfile -ExecutionPolicy Bypass -File %PSScriptPath%
--- start_server_script.ps1
+++ start_server_script.ps1
@@ -1,10 +1,6 @@
-# Read the PROJECT_DIR_PATH location from .env
-$PROJECT_DIR_PATH = (Get-Content .env | Where-Object { $_ -match '^PROJECT_DIR_PATH=' }) -replace '^PROJECT_DIR_PATH=', '' -replace '"', ''
-
-$env:PYTHONPATH = $PROJECT_DIR_PATH
-$serverScriptPath = "$PROJECT_DIR_PATH/src/core/server.py"
-
-Set-Location $env:PYTHONPATH
-& .\venv\Scripts\Activate.ps1
+$env:PYTHONPATH = $PSScriptRoot
+$serverScriptPath = "$PSScriptRoot/src/core/server.py"
+Set-Location $PSScriptRoot
+& .\.venv\Scripts\Activate.ps1
 py $serverScriptPath
 
